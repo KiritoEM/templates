1f240847e77e9146ac24828dc0fd1949870d4573
KiritoEM <johankirito64@gmail.com>
2024-07-21 13:08:35 +0300

    features:uptade .env

--- nodeJS-template/src/core/DatabaseConfig.js
+++ nodeJS-template/src/core/DatabaseConfig.js
@@ -4,7 +4,7 @@ import { databaseErrorHandler } from "./error.js";
 const connectDatabase = async () => {
     try {
         await mongoose.connection.close();
-        const uri = process.env.MONGO_COMPASS_URI || "";
+        const uri = process.env.MONGODB_URI_LOCAL || "";
         mongoose.set("strictQuery", false);
         await mongoose.connect(uri);
         console.log(`Connected to the database ${mongoose.connection.db.databaseName.toUpperCase()} ✅`);
